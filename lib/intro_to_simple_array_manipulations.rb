832f28234d2a03375bcc5ffc686f2a557b79b580
Christopher Dent <christopherdent01@gmail.com>
2019-06-21 02:48:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,5 +33,7 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
   
-def using_insert(list_of_programming_languages, "")
+def using_insert(list_of_programming_languages, "Python")
+  list_of_programming_languages.insert("Python")
+end
   
