2a6957afd044f872204e6cd0b612a06a5ae8534d
Christopher Dent <christopherdent01@gmail.com>
2019-06-21 02:37:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,8 +29,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array, element)
-  all_my_favs = concat(my_favorite_things, more_favs)
+def using_concat(array)
+  all_my_favs = my_favorite_things + more_favs
 end
   
   
